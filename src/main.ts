a1a90922bfc2dc5aedf0abd419c030f88323d3c7
Nenad <nbursa@gmail.com>
2020-11-08 12:30:55 +0100

    init firebase

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,10 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import Calendar from 'v-calendar'
-// import Calendar from 'v-calendar'
-// import DatePicker from 'v-calendar'
 
 Vue.config.productionTip = false
 
 Vue.use(Calendar, { componentPrefix: 'nox' })
-// Vue.component('calendar', Calendar)
-// Vue.component('date-picker', DatePicker)
 
 new Vue({
   router,
