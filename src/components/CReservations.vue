a15a7e6ff5a61a8aeff799b7b26631190318521d
Nenad <nbursa@gmail.com>
2020-11-12 00:44:57 +0100

    rf reservations sidebar

--- src/components/CReservations.vue
+++ src/components/CReservations.vue
@@ -24,111 +24,106 @@
 </template>
 
 <script lang="ts">
-// import VDatePicker from 'v-calendar/lib/components/date-picker.umd'
 import { Component, Vue } from 'vue-property-decorator'
 
-@Component({
-  components: {
-    // VDatePicker
-  }
-})
+@Component({})
 export default class VReservations extends Vue {
   private date = new Date().toISOString().substr(0, 10)
   private startTime = new Date()
   private endTime = new Date()
 
-  private focus = ''
-  private type = 'month'
-  private typeToLabel = {
-    month: 'Month',
-    week: 'Week',
-    day: 'Day',
-    '4day': '4 Days'
-  }
+  // private focus = ''
+  // private type = 'month'
+  // private typeToLabel = {
+  //   month: 'Month',
+  //   week: 'Week',
+  //   day: 'Day',
+  //   '4day': '4 Days'
+  // }
 
-  private selectedEvent = {}
-  private selectedElement = null
-  private selectedOpen = false
-  private events = []
-  private colors = ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1']
-  private names = ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party']
+  // private selectedEvent = {}
+  // private selectedElement = null
+  // private selectedOpen = false
+  // private events = []
+  // private colors = ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1']
+  // private names = ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party']
 
-  mounted () {
-    this.$refs.calendar.checkChange()
-  }
+  // mounted () {
+  //   this.$refs.calendar.checkChange()
+  // }
 
-  viewDay ({ date }: {date: string}) {
-    this.focus = date
-    this.type = 'day'
-  }
+  // viewDay ({ date }: {date: string}) {
+  //   this.focus = date
+  //   this.type = 'day'
+  // }
 
-  getEventColor (event: { color: string }) {
-    return event.color
-  }
+  // getEventColor (event: { color: string }) {
+  //   return event.color
+  // }
 
-  setToday () {
-    this.focus = ''
-  }
+  // setToday () {
+  //   this.focus = ''
+  // }
 
-  prev () {
-    this.$refs.calendar.prev()
-  }
+  // prev () {
+  //   this.$refs.calendar.prev()
+  // }
 
-  next () {
-    this.$refs.calendar.next()
-  }
+  // next () {
+  //   this.$refs.calendar.next()
+  // }
 
-  // eslint-disable-next-line
-  showEvent ({ nativeEvent, event }: { nativeEvent: any; event: any}) {
-    console.log('show event: ', nativeEvent, event)
-    const open = () => {
-      this.selectedEvent = event
-      this.selectedElement = nativeEvent.target
-      setTimeout(() => {
-        this.selectedOpen = true
-      }, 10)
-    }
+  // // eslint-disable-next-line
+  // showEvent ({ nativeEvent, event }: { nativeEvent: any; event: any}) {
+  //   console.log('show event: ', nativeEvent, event)
+  //   const open = () => {
+  //     this.selectedEvent = event
+  //     this.selectedElement = nativeEvent.target
+  //     setTimeout(() => {
+  //       this.selectedOpen = true
+  //     }, 10)
+  //   }
 
-    if (this.selectedOpen) {
-      this.selectedOpen = false
-      setTimeout(open, 10)
-    } else {
-      open()
-    }
+  //   if (this.selectedOpen) {
+  //     this.selectedOpen = false
+  //     setTimeout(open, 10)
+  //   } else {
+  //     open()
+  //   }
 
-    nativeEvent.stopPropagation()
-  }
+  //   nativeEvent.stopPropagation()
+  // }
 
-  updateRange ({ start, end }: { start: { date: Date }; end: { date: Date } }) {
-    const events = []
+  // updateRange ({ start, end }: { start: { date: Date }; end: { date: Date } }) {
+  //   const events = []
 
-    const min = new Date(`${start.date}T00:00:00`)
-    const max = new Date(`${end.date}T23:59:59`)
-    const days = (max.getTime() - min.getTime()) / 86400000
-    const eventCount = this.rnd(days, days + 20)
+  //   const min = new Date(`${start.date}T00:00:00`)
+  //   const max = new Date(`${end.date}T23:59:59`)
+  //   const days = (max.getTime() - min.getTime()) / 86400000
+  //   const eventCount = this.rnd(days, days + 20)
 
-    for (let i = 0; i < eventCount; i++) {
-      const allDay = this.rnd(0, 3) === 0
-      const firstTimestamp = this.rnd(min.getTime(), max.getTime())
-      const first = new Date(firstTimestamp - (firstTimestamp % 900000))
-      const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
-      const second = new Date(first.getTime() + secondTimestamp)
+  //   for (let i = 0; i < eventCount; i++) {
+  //     const allDay = this.rnd(0, 3) === 0
+  //     const firstTimestamp = this.rnd(min.getTime(), max.getTime())
+  //     const first = new Date(firstTimestamp - (firstTimestamp % 900000))
+  //     const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
+  //     const second = new Date(first.getTime() + secondTimestamp)
 
-      events.push({
-        name: this.names[this.rnd(0, this.names.length - 1)],
-        start: first,
-        end: second,
-        color: this.colors[this.rnd(0, this.colors.length - 1)],
-        timed: !allDay
-      })
-    }
+  //     events.push({
+  //       name: this.names[this.rnd(0, this.names.length - 1)],
+  //       start: first,
+  //       end: second,
+  //       color: this.colors[this.rnd(0, this.colors.length - 1)],
+  //       timed: !allDay
+  //     })
+  //   }
 
-    this.events = events as never
-  }
+  //   this.events = events as never
+  // }
 
-  rnd (a: number, b: number) {
-    return Math.floor((b - a + 1) * Math.random()) + a
-  }
+  // rnd (a: number, b: number) {
+  //   return Math.floor((b - a + 1) * Math.random()) + a
+  // }
 }
 </script>
 
@@ -168,6 +163,7 @@ export default class VReservations extends Vue {
             .v-btn
               font-size 1.2rem
               height 38px
+              margin-bottom 0
       &--time
         margin-bottom 20px
         .v-picker__body
